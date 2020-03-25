ea8382c5a50cc934f9d885944f5331e95b400dbe
Name <alecli2014@gmail.com>
2020-03-25 02:10:35 +0000

    got code

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var custom_api_url = https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC;
 
-$("#search-button").click(function(){
-  
-  
-  
+$("#search-button").click(function(){ 
+    fetch(custom_api_url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+         $(".text-center").html(`<img src="${data.data[0].images.downsized_large.url}"><img>`);
+      })
 });
+
 
